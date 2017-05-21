60c9916f828d037d79deacce2ad270d3e52b88a1
mpiask <mpiask@gmail.com>
2017-05-21 13:34:03 +0200

    two html pages, no back end

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+    def index
+    end
+    def compose
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
     get 'welcome/index'
     root 'welcome#index'
+    get 'compose' => 'welcome#compose'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
