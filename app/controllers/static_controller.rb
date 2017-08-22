42100927fd7dd75a7f1f8b9db33d20ffbfef79ce
mpiask <mpiask@gmail.com>
2017-08-22 11:01:12 +0200

    empty mix controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,22 @@
 class StaticController < ApplicationController
 	def start
 	end
+
 	def home
 	end
+
 	def about
 	end
+
+	def select
+		#redirect_to order_path if order_exists?
+		@baseherbs = Herb.where(base: true).take(3)
+	end
+
 	def herbalab
 		@herbs = Herb.all
 	end
+
 	def showherb
 		@herb = Herb.find(params[:id])
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#start'
-
   get 'home', to: 'static#home'
-
   get 'about', to: 'static#about'
-
   get 'herbalab', to: 'static#herbalab'
-
   get 'herbalab/:id', to: 'static#showherb'
-
-  get 'select', to: 'mix#select'
-
-  get 'order', to: 'mix#order'
+  get 'select', to: 'static#select'
 
   resources :jars, only: [:new, :create]
+  resources :orders
 
 end
