c1bdf0fc707c7c8f10967146da74de0ea9e97114
mpiask <mpiask@gmail.com>
2017-08-24 18:35:06 +0200

    rebuild sliders

--- app/assets/javascripts/pot.js
+++ app/assets/javascripts/pot.js
@@ -1,87 +1,87 @@
 var herbeh = 0; //px //herb 'empty' base height
 var potheight = 1; //px*0.01
 var baseperc = 40; //% of base herb
-var perc = [10,10,10,10,baseperc]
 var cap = 0;
-
-
-function createslider(i, val){
-    $("#slider"+i).slider({
-        min: 0, max: 60, value: val,
-        slide: function(event, ui) {
-            if(ui.value >= cap && ui.value > perc[i]) {
-                perc[i] = cap;
-                showperc(perc[i]);
-                updateheights();
-                return false;
-            }
-
-            showperc(ui.value);
-            perc[i]=ui.value;
-            updateheights();
-            return true;
-        },
-        start: function(event, ui) {
-            cap = 100 - perc.reduce(add, 0) + perc[i];
-            showperc(ui.value);
-            $("#potperc").stop(true,true).fadeIn("fast");
-        },
-        stop: function(event, ui) {
-            $("#potperc").stop(true,true).fadeOut("fast");
-            $("input#quantity"+i).val(perc[i]);
-            if(perc.reduce(add, 0) >= 100) {
-                $("#orderbutton").addClass("ready");
-                $("#ordercircle").addClass("ready");
-            } else {
-                $("#orderbutton").removeClass("ready");
-                $("#ordercircle").removeClass("ready");
-            };
-        }
-    }) //slider end
-};
-
-function showperc(perc){
-  document.getElementById("potperc").innerHTML = perc+"%";
-}
+var availableTotal = 100 - baseperc;
+var init_value = 15;
 
 function updatesliders(){
-    for(i=0;i<=3;i++){
-        createslider(i, perc[i]);
-    };
+    var sliders = $(".slider");
+    sliders.each(function() {
+      var id = $(this).attr('id').slice(-1);
+      $(this).slider({
+          min: 0, max: availableTotal, value: init_value, step: 1,
+          slide: function(event, ui) {
+
+              var total = 0;
+
+              sliders.not(this).each(function() {
+                  total += $(this).slider("option", "value");
+              });
+              total += ui.value;
+
+              var delta = availableTotal - total;
+
+
+              sliders.not(this).each(function() {
+                  var t = $(this),
+                      value = t.slider("option", "value");
+
+                  var new_value = value + (delta/2);
+
+                  if (new_value < 0 || ui.value == 100)
+                      new_value = 0;
+                  if (new_value > 100)
+                      new_value = 100;
+
+                  t.siblings('.value').text(new_value);
+                  t.slider('value', new_value);
+
+                  updateheight(t.attr('id').slice(-1), new_value)
+              });
+              total = 0;
+              sliders.each(function() {
+                  total += $(this).slider("option", "value");
+              });
+              updateheight(id, ui.value);
+              return true;
+          },
+          start: function(event, ui) {
+              $("#potperc").stop(true,true).fadeIn("fast");
+          },
+          stop: function(event, ui) {
+              $("#potperc").stop(true,true).fadeOut("fast");
+              $("input#quantity"+id).val(ui.value);
+          }
+      })
+    });
 };
 
-function updateheights(){
-    var i = 0;
-    var sum = 0;
-
-    for(i=0;i<=4;i++){
-        $("#herb"+i).css("height",perc[i]*potheight+"%");
-        sum += perc[i];
-    }
-    $("#herbe").css("height",herbeh+(100-sum)*potheight+"%")
+function updateheight(id, val){
+    $("#herb"+id).css("height",val*potheight+"%");
+    document.getElementById("percent"+id).innerHTML = val+"%";
+    document.getElementById("potperc").innerHTML = val+"%";
 };
 
+function fillthepot() {
+  for(i=0;i<=3;i++){
+    updateheight(i, init_value)
+  }
+  $("#herb"+4).css("height",baseperc*potheight+"%");
+  document.getElementById("percent4").innerHTML = baseperc+"%";
+  $("#herbe").css("height",0*potheight+"%");
+}
 
 function showherbdesc(description, color) {
-    //document.getElementById("descriptionpopup").style.display = "table-cell";
     document.getElementById("descriptionpopup").innerHTML = description;
     document.getElementById("descriptionpopup").style.background = color.replace("rgb(", "rgba(").replace(")", ", 0.5)");
     document.getElementById("descriptionpopup").style.border = "2px solid " + color;
     $("#descriptionpopup").stop(true,true).fadeIn(100).css("display","table-cell");
 }
 function hide(){
-    //document.getElementById("descriptionpopup").style.display = "none";
     $("#descriptionpopup").stop(true,true).fadeOut(100);
 }
 
-function add(a, b) {
-    return a + b;
-}
-
 function orderbutton() {
-    if(perc.reduce(add, 0) == 100) {
-        $("form").submit();
-    } else {
-        window.alert('Najpierw napełnij dzbanek!');
-    }
+    $("form").submit();
 }
