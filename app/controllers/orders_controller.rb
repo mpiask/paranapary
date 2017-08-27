14fd3dfcb847e1a5550f00359770a1e035a74755
mpiask <mpiask@gmail.com>
2017-08-27 18:36:52 +0200

    create edit order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
     def edit
       redirect_to select_path unless order_exists?
+      @order = current_order
       @jars = Jar.where(order_id: current_order.id)
     end
 end
