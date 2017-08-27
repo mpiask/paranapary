4e4efa65f1ce520661de4fbde2a9cb373bb7e77c
mpiask <mpiask@gmail.com>
2017-08-27 20:19:20 +0200

    created show order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,26 @@
 class OrdersController < ApplicationController
-    def edit
-      redirect_to select_path unless order_exists?
-      @order = current_order
-      @jars = Jar.where(order_id: current_order.id)
+  def edit
+    redirect_to select_path unless order_exists?
+    @order = current_order
+    @jars = Jar.where(order_id: current_order.id)
+  end
+
+  def update
+  	@order = current_order
+  	if @order.update_attributes(order_params)
+      flash[:success] = "Profile updated"
+      redirect_to current_order
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def show
+    @order = current_order
+  end
+
+  private
+    def order_params
+      params.require(:order).permit(:name, :surname, :email, :street, :zipcode, :city, :phone)
     end
 end
