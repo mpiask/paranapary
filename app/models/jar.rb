c5b78bdc7b53e6091dc0981be00b30e28757a76e
mpiask <mpiask@gmail.com>
2017-09-01 19:08:14 +0200

    jar validation

--- app/models/jar.rb
+++ app/models/jar.rb
@@ -1,4 +1,19 @@
 class Jar < ApplicationRecord
   belongs_to :order
   has_many :ingredients, dependent: :delete_all
+
+  # validate :must_be_full
+
+  def fullness
+    self.ingredients.map {|x| x[:quantity] }.sum
+  end
+  def isfull?
+    return true if self.fullness == 100
+    return false
+  end
+  def must_be_full
+    unless isfull?
+      errors.add(:jar, "musi być pełny")
+    end
+  end
 end
